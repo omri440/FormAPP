8c50f0154485f5e5da9dd5578b52f6e2aceef3d3
omriassulin <omri440@gmail.com>
2026-03-07 14:03:42 +0200

    little fixes

--- FormAppClient/src/app/features/user/layout/user-layout.ts
+++ FormAppClient/src/app/features/user/layout/user-layout.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Auth } from '../../../core/services/auth';
 
@@ -13,15 +13,10 @@ export class UserLayout {
   private auth = inject(Auth);
 
   user = this.auth.currentUser;
-  isMenuOpen = signal(false);
 
   get userInitials(): string {
     const email = this.user()?.email ?? '';
     return email.slice(0, 2).toUpperCase();
-  }
-
-  toggleMenu(): void {
-    this.isMenuOpen.update(v => !v);
   }
 
   logout(): void {
